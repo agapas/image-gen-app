1618c939d07a5abc3060bf9f1d6a00fecd22fc89
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2023-04-25 16:52:20 +0100

    Added app skeleton

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <h2>Create images with your mind</h2>
+      <textarea
+        className="app-input"
+        placeholder="Create any type of image with added description. Use as much details as you like."
+      />
+      <button>Generate Image</button>
+    </>
+  );
 }
