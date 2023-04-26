a3f44f0d103304318fc35edbe8d86b78495ccb63
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2023-04-26 17:04:35 +0100

    Added basic styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 import type { AppProps } from "next/app";
+import "./App.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className="app">
       <h2>Create images with your mind</h2>
       <textarea
         className="app-input"
         placeholder="Create any type of image with added description. Use as much details as you like."
       />
       <button>Generate Image</button>
-    </>
+    </div>
   );
 }
