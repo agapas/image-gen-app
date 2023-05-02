0941daa10dbda95598a17ebc10ff41c229bae308
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2023-05-02 16:16:51 +0100

    Installed openai and configured image generation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,45 @@
 import type { AppProps } from "next/app";
+import getConfig from "next/config";
+import Image from "next/image";
+import { useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
+import imageIcon from "../assets/imageIcon.png";
 import "./App.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [imageResult, setImageResult] = useState(imageIcon);
+
+  const { publicRuntimeConfig } = getConfig();
+  const apiKey =
+    typeof publicRuntimeConfig !== "undefined" && publicRuntimeConfig.apiKey
+      ? publicRuntimeConfig.apiKey
+      : process.env.API_KEY;
+
+  if (!apiKey) {
+    throw new Error("apiKey is not defined");
+  }
+
+  const configuration = new Configuration({ apiKey });
+  const openai = new OpenAIApi(configuration);
+
+  const generateImage = async () => {
+    const response = await openai.createImage({
+      prompt: "dummy image",
+      n: 1,
+      size: "1024x1024",
+    });
+    console.log("response data: ", response.data);
+  };
+
   return (
     <div className="app">
-      <h2>Create images with your mind</h2>
+      <h2>Create images with your mind and DALL-E</h2>
       <textarea
         className="app-input"
-        placeholder="Create any type of image with added description. Use as much details as you like."
+        placeholder="Create any type of image from your description. Use as much details as you like."
       />
-      <button>Generate Image</button>
+      <button onClick={generateImage}>Generate Image</button>
+      <Image src={imageResult} alt="Image result" width={256} height={256} />
     </div>
   );
 }
