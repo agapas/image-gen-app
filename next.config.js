3d242b23030f7870ec679d738ddbd389a21fe78a
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2023-05-04 11:38:38 +0100

    Fixed errors and made openai api call work

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
+  images: {
+    remotePatterns: [{
+      protocol: "https",
+      hostname: "oaidalleapiprodscus.blob.core.windows.net",
+      pathname: "/**",
+    }]
+  },
   publicRuntimeConfig: {
     apiKey: process.env.API_KEY
   }
